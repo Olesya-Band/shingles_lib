e7c84bad2f108328fb13371b8e5015d5174a19dc
Olesya-Band <55530203+Olesya-Band@users.noreply.github.com>
2020-05-09 22:19:26 +0300

    Update SynonymReplacer.java

--- src/main/java/com/nlp/SynonymReplacer.java
+++ src/main/java/com/nlp/SynonymReplacer.java
@@ -3,20 +3,9 @@ package com.nlp;
 import java.io.*;
 import java.util.*;
 
-/**
- * Класс, предназначенный для замены слов в тексте на их синонимы.
- */
 public class SynonymReplacer {
-    /**
-     * Отображение синонимов, ключ - слово, значение - его синоним.
-     */
     private Map<String, String> synonyms;
 
-    /**
-     * Конструктор, принмает на вход файл, в котором лежат последовательности
-     * синонимов, и символ разделитель для синонимов. Каждая последовательность синонимов
-     * должна быть расположена на отдельной строке.
-     */
     public SynonymReplacer(String filePath, String separator) {
         synonyms = new HashMap<>();
 
@@ -37,19 +26,12 @@ public class SynonymReplacer {
         }
     }
 
-    /**
-     * Конструктор, принмает на вход отображение для синонимов.
-     */
     public SynonymReplacer(Map<String, String> synonyms) {
         checkSynonyms(synonyms);
 
         this.synonyms = new HashMap<>(synonyms);
     }
 
-    /**
-     * Метод, выполняет проверку слов и их синонимов на отсутствие null.
-     * @throws NullPointerException - если слово или его синоним = null.
-     */
     private void checkSynonyms(Map<String, String> synonyms) {
         Objects.requireNonNull(synonyms, "synonyms");
 
@@ -63,30 +45,18 @@ public class SynonymReplacer {
         return new HashMap<>(synonyms);
     }
 
-    /**
-     * Методб озвращает синоним для определенного слова или если синоним не найден - само это слово.
-     */
     public String getSynonym(String word) {
         return synonyms.getOrDefault(word, word);
     }
 
-    /**
-     * Метод, возвращает список синонимов для слов текста в порядке их следования в тексте.
-     */
     public List<String> replace(String text) {
         return replace(text.split(" "));
     }
 
-    /**
-     * Метод, возвращает список синонимов для слов массива в порядке их следования в массиве.
-     */
     public List<String> replace(String[] words) {
         return replace(Arrays.asList(words));
     }
 
-    /**
-     * Метод, возвращает список синонимов для слов списка в порядке их следования в списке.
-     */
     public List<String> replace(List<String> words) {
         for (int i = 0; i < words.size(); ++i) {
             words.set(i, getSynonym(words.get(i)));
