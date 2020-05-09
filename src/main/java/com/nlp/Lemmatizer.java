51df8100bb241775b69a19467ddf7bbfd335d1a3
Olesya-Band <55530203+Olesya-Band@users.noreply.github.com>
2020-05-09 22:17:43 +0300

    Update Lemmatizer.java

--- src/main/java/com/nlp/Lemmatizer.java
+++ src/main/java/com/nlp/Lemmatizer.java
@@ -5,11 +5,6 @@ import ru.textanalysis.tawt.sp.api.SyntaxParser;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Множество с единственным экземпляром. Предназначенно для получения лемм последовательности слов.
- * Поскольку каждый раз грузить 500мб данных в оперативку при создании нового экземпляра SyntaxParser - это не очень хорошо.
- * Было сделано это множество, содержащее единственный экземпляр объекта, с помощью которого можно получать леммы.
- */
 public enum Lemmatizer {
     INSTANCE;
 
@@ -20,29 +15,14 @@ public enum Lemmatizer {
         parser.init();
     }
 
-    /**
-     * Метод, предназначенный для получения лемм слов.
-     * @param words- массив, содержащий слова, леммы которых нужно получить.
-     * @return - список лемм переданных слов, в порядке их следования в массиве.
-     */
     public List<String> getLemms(String[] words) {
         return getLemms(String.join(" ", words));
     }
 
-    /**
-     * Метод, предназначенный для получения лемм слов.
-     * @param words - последовательность, содержащая слова, леммы которых нужно получить.
-     * @return - список лемм переданных слов, в порядке их следования в последовательности.
-     */
     public List<String> getLemms(Iterable<String> words) {
         return getLemms(String.join(" ", words));
     }
 
-    /**
-     * Метод, предназначенный для получения лемм слов.
-     * @param text - строка, содержащая слова, леммы которых нужно получить.
-     * @return - список лемм переданных слов, в порядке их следования в строке.
-     */
     public List<String> getLemms(String text) {
         var treeSentence = parser.getTreeSentence(text);
         var lemms = new ArrayList<String>();
